a86adbee2353ef723f525e647d0345ad0f6c1496
Ruan Araujo <ruanaraujo8@gmail.com>
2022-01-11 15:30:47 -0300

    Adicionando estrutura de pastas

---
+++ src/main/java/model/Users.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+
+    private String nome;
+    private String cpf;
+    List<Reserva> listaReservas = new ArrayList<>();
+
+}
