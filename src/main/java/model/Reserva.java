492af585bab0dfee167da62e019f47de45176067
Ruan Araujo <ruanaraujo8@gmail.com>
2022-01-12 10:11:08 -0300

    adicionando campo id contrutor

--- src/main/java/model/Reserva.java
+++ src/main/java/model/Reserva.java
@@ -2,13 +2,15 @@ package model;
 
 public class Reserva {
 
+    private Integer id;
     private Rotas rota;
     private String metodoPagamento;
     private Integer totalDePassagens;
 
     public Reserva(){}
 
-    public Reserva(Rotas rota, String metodoPagamento, Integer totalDePassagens) {
+    public Reserva(Integer id, Rotas rota, String metodoPagamento, Integer totalDePassagens) {
+        this.id = id;
         this.rota = rota;
         this.metodoPagamento = metodoPagamento;
         this.totalDePassagens = totalDePassagens;
--- src/main/java/model/Rotas.java
+++ src/main/java/model/Rotas.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 public class Rotas {
 
+    private Integer id;
     private String nome;
     private BigDecimal valor;
     private Date dataDeIda;
@@ -12,11 +13,11 @@ public class Rotas {
 
     public Rotas(){}
 
-    public Rotas(String nome, BigDecimal valor, Date dataDeIda, Date dataDeVolta) {
+    public Rotas(Integer id, String nome, BigDecimal valor, Date dataDeIda, Date dataDeVolta) {
+        this.id = id;
         this.nome = nome;
         this.valor = valor;
         this.dataDeIda = dataDeIda;
         this.dataDeVolta = dataDeVolta;
     }
-    
 }
